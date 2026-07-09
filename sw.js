b0bc2ca1998d36509e86992d9b32be45fda0a974
JoseLucianoCaamal <caamalayala8@gmail.com>
2026-07-09 16:13:07 -0600

    2.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'drive-estudiantil-v3.1';
+const CACHE_NAME = 'drive-estudiantil-v3.2';
 const urlsToCache = ['./', './index.html', './app.js', './manifest.json', './Img/akko.jpeg'];
 
 self.addEventListener('install', event => {
